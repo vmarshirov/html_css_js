d4a76cf5c11dbee2563cffcce348cc05566ffa3b
vmarshirov <vmarshirov@outlook.com>
2022-09-16 20:08:11 +0300

    test_JS

---
+++ test/js/spec_sections.js
@@ -0,0 +1,21 @@
+/*
+
+const header_ch_0 = document.querySelector("header").children[0]
+console.log(header_ch_0.innerHTML)
+alert("Оповещение")
+header_ch_0.innerHTML = "new_innerHTML"
+header_ch_0.style.color='red'
+console.log(header_ch_0.innerHTML)
+*/
+/*
+
+
+const time_0= document.querySelectorAll("time")[0]
+console.log(time_0.attributes)
+console.log(time_0.attributes[0].name)
+console.log(time_0.attributes[0].value)
+console.log(time_0.attributes[1].name)
+console.log(time_0.attributes[1].value)
+time_0.attributes[1].value='new_datePublished'
+console.log(time_0.attributes[1].value)
+*/
