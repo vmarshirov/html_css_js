62e078fce76aca3cfcaa9125a11e404f5d21a1b6
vmarshirov <vmarshirov@outlook.com>
2022-09-21 18:51:11 +0300

    main__css_js_00

---
+++ css_js_00/js/index.js
@@ -0,0 +1,7 @@
+const em_0 = document.querySelectorAll("em")[0]
+em_0.innerText = "очень рад"
+em_0.style.fontSize= '1.5rem'
+
+
+const em_1 = document.querySelectorAll('span')[1]
+em_1.style = 'background-color: brown; color: red;'
