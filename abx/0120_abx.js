a0f9c43aebe41a235f046621d7baa8fafde4cf05
vmarshirov <vmarshirov@outlook.com>
2022-11-09 15:23:56 +0300

    bootstrap JS

---
+++ abx/0120_abx.js
@@ -0,0 +1,43 @@
+function checkFields(form) {
+    const elems = form.elements
+    console.log("elems:", elems)
+    console.log("elems[0]: ", elems[0])
+    console.log("elems[0].name: ", elems[0].name)
+    console.log("elems[0].value: ", elems[0].value)
+    console.log("elems.a.value: ", elems.a.value)
+
+    console.log("typeof (elems.a.value): ", typeof (elems.a.value))
+    let aFloat = parseFloat(elems.a.value)
+    let bFloat = parseFloat(elems.b.value)
+    let xFloat = parseFloat(elems.x.value)
+
+    if (aFloat < bFloat) {
+        var low = aFloat
+        var high = bFloat
+    } else {
+        var low = bFloat
+        var high = aFloat
+    }
+    console.log(low)
+    console.log(high)
+    console.log(xFloat)
+
+
+    if (aFloat <= xFloat && xFloat <= bFloat) {
+        var result = "X принадлежит заданному промежутку"
+        var check = true
+    } else {
+        var result = "X не принадлежит заданному промежутку"
+        var check = false
+    }
+
+    let result_p = document.getElementById('result')
+    result_p.innerHTML = "Ответ: " + result
+
+
+    if (check) {
+        document.getElementById("UserEnter").submit()
+    } else {
+        alert("Ответ\n" + result + "\nПовторите ввод")
+    }
+}
